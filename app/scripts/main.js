7fb6835e73f675142293e2e124c1e777b0ea47a1
nrgized <energieee@gmail.com>
2015-04-13 22:41:17 +0300

    updated 2015-04-13

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,14 @@
 console.log('\'Allo \'Allo!');
 
+
 $('#menu').click(function(){
+	'use strict';
 	$('#menu').toggleClass('active');
+
 });
+
+$('#slider').slick({
+	nextArrow: '<a class="next"></a>',
+	prevArrow: '<a class="prev"></a>'
+});
+
